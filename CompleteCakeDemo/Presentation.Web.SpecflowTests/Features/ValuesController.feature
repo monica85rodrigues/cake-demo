4dba816b973a6ff09624058dd9e0ce3c018f9b49
Monica Rodrigues <monica85rodrigues@gmail.com>
2017-06-22 22:30:42 +0100

    add first specflow tests

---
+++ CompleteCakeDemo/Presentation.Web.SpecflowTests/Features/ValuesController.feature
@@ -0,0 +1,7 @@
+﻿Feature: Values
+
+@mytag
+Scenario: There are two values in the system
+	Given there are two values in the system
+	When I retrieve list of all values
+	Then I get a list with two values
---
+++ CompleteCakeDemo/Presentation.Web.SpecflowTests/Steps/ValuesControllerSteps.cs
@@ -0,0 +1,26 @@
+﻿namespace Presentation.Web.SpecflowTests.Steps
+{
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public sealed class ValuesControllerSteps
+    {
+        [Given(@"there are two values in the system")]
+        public void GivenThereAreTwoValuesInTheSystem()
+        {
+            //to be implemented
+        }
+
+        [When(@"I retrieve list of all values")]
+        public void WhenIRetrieveListOfAllValues()
+        {
+            //to be implemented
+        }
+
+        [Then(@"I get a list with two values")]
+        public void ThenIGetAListWithTwoValues()
+        {
+            //to be implemented
+        }
+    }
+}
